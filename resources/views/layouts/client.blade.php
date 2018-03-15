487e5ee38816611afcebfd3731b7c7da3f7bd9ac
Roger Forner Fabre <sir.rogerforner@gmail.com>
2018-03-15 20:31:24 +0100

    Layout iniciat. Afegim Navbar en partials, creem un @yeld per a les meta etiquetes description i title, afegim Fontawesome, el Navbar té preparats enllaços a les dues bandes (dreta i esquerra), hi ha dropdowns d'exemple

---
+++ resources/views/layouts/client.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Informació del lloc web -->
+    <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="@yield('description')">
+
+    <!-- CSS -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+  </head>
+  <body>
+    {{-- Navbar --}}
+    @include('layouts.partials.navigation.navbar')
+
+    <!-- Contingut -->
+    @yield('content')
+
+    <!-- JavaScript -->
+    <script src="{{ asset('js/app.js') }}"></script>
+  </body>
+</html>
