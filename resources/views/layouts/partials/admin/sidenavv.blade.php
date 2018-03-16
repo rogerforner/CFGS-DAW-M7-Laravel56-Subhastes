cc21662696865a67f42748c9a9e8ec003ae3d420
Roger Forner Fabre <hello@rogerforner.com>
2018-03-16 12:44:16 +0100

    Sidebar Nav OK

--- resources/views/layouts/partials/admin/sidenavv.blade.php
+++ resources/views/layouts/partials/admin/sidenavv.blade.php
@@ -1,38 +1,38 @@
 <nav class="nav flex-column">
   {{-- Enllaç Normal --}}
-  <a class="nav-link text-white" href="{{ url('/admin') }}"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+  <a class="nav-link" href="{{ url('/admin') }}"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
 
   {{-- Enllaç "Dropdown" (Usuaris) --}}
-  <a class="nav-link text-white"
+  <a class="nav-link"
      data-toggle="collapse" href="#collapseUsers" aria-expanded="false"
      aria-controls="collapseUsers">
     <i class="fas fa-users"></i> Usuaris
   </a>
   <div class="collapse" id="collapseUsers">
-    <li><a class="nav-link text-white" href="#">Llistar</a></li>
-    <li><a class="nav-link text-white" href="#">Crear</a></li>
+    <li><a class="nav-link" href="#">Llistar</a></li>
+    <li><a class="nav-link" href="#">Crear</a></li>
   </div>
 
   {{-- Enllaç "Dropdown" (Rols) --}}
-  <a class="nav-link text-white"
+  <a class="nav-link"
      data-toggle="collapse" href="#collapseRoles" aria-expanded="false"
      aria-controls="collapseRoles">
     <i class="fas fa-user-secret"></i> Rols
   </a>
   <div class="collapse" id="collapseRoles">
-    <li><a class="nav-link text-white" href="#">Llistar</a></li>
-    <li><a class="nav-link text-white" href="#">Crear</a></li>
+    <li><a class="nav-link" href="#">Llistar</a></li>
+    <li><a class="nav-link" href="#">Crear</a></li>
   </div>
 
   {{-- Enllaç "Dropdown" (Rols) --}}
-  <a class="nav-link text-white"
+  <a class="nav-link"
      data-toggle="collapse" href="#collapseAuctions" aria-expanded="false"
      aria-controls="collapseAuctions">
     <i class="fas fa-euro-sign"></i> Subhastes
   </a>
   <div class="collapse" id="collapseAuctions">
-    <li><a class="nav-link text-white" href="#">Llistar</a></li>
-    <li><a class="nav-link text-white" href="#">Crear</a></li>
+    <li><a class="nav-link" href="#">Llistar</a></li>
+    <li><a class="nav-link" href="#">Crear</a></li>
   </div>
 </nav>
 
