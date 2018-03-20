e5d4483ffbe0b17b5984623620438cff3c86fbda
Roger Forner Fabre <sir.rogerforner@gmail.com>
2018-03-20 19:56:03 +0100

    Directori per a les vistes de les Categories; Rutes organitzades i afegida la de categories;

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,38 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-// Pàgina d'inici del lloc web.
+# Pàgina d'inici
+*******************************************************************************/
 Route::get('/', function () {
     return view('index');
 });
 
-// Pàgina d'inici del panell d'administració.
-Route::get('/admin', function () {
-    return view('admin.index');
-});
-
+/*
+# Auth
+*******************************************************************************/
 Auth::routes();
 
+/*
+# Home
+TODO: afegir a admin
+*******************************************************************************/
 Route::get('/home', 'HomeController@index')->name('home');
-Route::group(['prefix'=>'admin'], function(){
-    Route::resource('products','ProductController');
+
+/*
+# Administració
+*******************************************************************************/
+Route::group(['prefix'=>'admin'], function() {
+  Route::get('/', function () {
+    return view('admin.index');
+  });
+
+  Route::resource('categories','VategoryController');
+  Route::resource('products','ProductController');
+});
+
+/*
+# Client
+*******************************************************************************/
+Route::group(['prefix'=>'client'], function() {
+
 });
