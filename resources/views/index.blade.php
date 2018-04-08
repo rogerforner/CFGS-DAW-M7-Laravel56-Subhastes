bbe4d6f549358d016ee1213d483e1ef0b9f279b1
Roger Forner Fabre <hello@rogerforner.com>
2018-04-08 17:13:41 +0200

    Layout Mostrar Subhastes actives i finalitzades OK

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,31 @@
 @section('description', 'Pàgina d\'inici')
 @section('content')
 
-{{-- Contingut aquí --}}
+<div class="container my-5">
+  <div class="row">
+    <div class="col">
+      <h2>Subhastes</h2>
+      <!-- Subhastes (navegació) -->
+      <nav class="nav nav-tabs" id="nav-tab" role="tablist">
+        <a class="nav-item nav-link active" id="nav-actives-tab" data-toggle="tab"
+           href="#nav-actives" role="tab" aria-controls="actives" aria-expanded="true">Actives</a>
+        <a class="nav-item nav-link" id="nav-finalitzades-tab" data-toggle="tab"
+           href="#nav-finalitzades" role="tab" aria-controls="finalitzades" aria-expanded="false">Finalitzades</a>
+      </nav>
+
+      <!-- Subhastes (contingut) -->
+      <div class="tab-content" id="nav-tabContent">
+        <!-- Actives -->
+        <div class="tab-pane fade active show" id="nav-actives" role="tabpanel" aria-labelledby="nav-actives-tab" aria-expanded="true">
+          <p>Actives</p>
+        </div>
+        <!-- Finalitzades -->
+        <div class="tab-pane fade" id="nav-finalitzades" role="tabpanel" aria-labelledby="nav-finalitzades-tab" aria-expanded="false">
+          <p>Finalitzades</p>
+        </div>
+      </div>
+    </div><!-- /.col -->
+  </div><!-- /.row -->
+</div><!-- /.container -->
 
 @endsection
