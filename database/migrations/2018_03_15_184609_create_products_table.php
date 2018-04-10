0fe6439660460c69d031bb28786c45457aafe8a7
joan_navarro <joanetnavarro98@gmail.com>
2018-04-10 19:04:04 +0200

    Eliminacions d'usuaris, productes i subhastes

--- database/migrations/2018_03_15_184609_create_products_table.php
+++ database/migrations/2018_03_15_184609_create_products_table.php
@@ -20,6 +20,7 @@ class CreateProductsTable extends Migration
             $table->string('characteristics');
             $table->string('image');
             $table->timestamps();
+            $table->boolean('active')->default(true);
         });
     }
 
